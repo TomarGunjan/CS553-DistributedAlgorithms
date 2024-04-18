cf52eef43f86bc8a6fd28bcdc568e1deb77c8657
gtomar2 <gunjan_tomar@ymail.com>
2024-04-17 21:35:51 -0500

    adding main file for centralized execution

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,7 +1,10 @@
 package main
 
+import akka.event.slf4j.Logger
 import main.algorithms.BrachaTouegAlgorithm
 
 object Main extends App{
+    val logger = Logger("Main")
+    logger.info("Running Bracha Toueg Algorithm")
     BrachaTouegAlgorithm.main()
 }
