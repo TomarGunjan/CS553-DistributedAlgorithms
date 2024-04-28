451b48d8612e4d071109065ab950fccad152a493
makaveli <agarwal.dhruv1234@gmail.com>
2024-04-28 09:25:50 -0500

    chang roberts election

--- src/main/scala/main/utility/ApplicationProperties.scala
+++ src/main/scala/main/utility/ApplicationProperties.scala
@@ -5,7 +5,6 @@ import com.typesafe.config.{Config, ConfigFactory}
 /**
  * Utility class to load data from application.conf file
  */
-
 object ApplicationProperties {
   private val app = ConfigFactory.load()
   val deadlockData : Config = app.getConfig("BrachaToueg.TestData.DeadlockTestData")
@@ -14,4 +13,6 @@ object ApplicationProperties {
   val echoInputFile: String = app.getString("EchoAlgorithm.InputFile")
   val treeInputFile: String = app.getString("TreeAlgorithm.InputFile")
   val snapshotInputFile: String = app.getString("SnapshotAlgorithm.InputFile")
+  val changRobertsData: Config = app.getConfig("ChangRoberts.TestData")
+
 }
